ea5e4ec756203f6653edc1437ed6aaf7beb0dc7d
ladentelle <dbilenkis@gm.slc.edu>
2014-12-10 12:58:36 -0500

    image added
    added image for the chrome store

--- conference_coding/js/script.js
+++ conference_coding/js/script.js
@@ -1,12 +1,18 @@
 
 $(document).ready(function() {
-	var saveTimeOut = 60000;
+	var saveTimeOut = 6000;
 	var saveInterval; 
 	var editContentElement = $(".mainText");  
 
 	editContentElement.popline({position: 'relative'});
 
 	var saveToStorage = function() {
+		var	editContent = editContentElement.html();
 
+		chrome.storage.sync.set({'editContentElement': editContent}, function() {
+          // Notify that we saved.
+          console.log('Settings saved');
+        });
 	} 
+	saveInterval = setInterval(saveToStorage, saveTimeOut);
 });
